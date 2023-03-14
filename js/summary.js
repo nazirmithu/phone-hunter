47c013f0d60b319c835e3245795e993ece3123d1
nazirmithu <nazirmithu22@gmail.com>
2023-03-15 00:53:19 +0600

    first commit

---
+++ js/summary.js
@@ -0,0 +1,25 @@
+/*
+FETCH 
+1. Fetch must provide url
+    ** dynamic or object
+2. how to convert fetch promise return to json 
+3. how to convert json to data 
+4.[cool headed]
+    ** Users:
+    --> array of object 
+    --> object with property users 
+    --> object with a property called deta
+    
+    nested object 
+
+    -----------------
+ DOM manipulation 
+ 1. get something from to the DOM
+ 2. create element to append to the DOM
+ 3. dynamically load data based on id
+
+ --------------------
+ array --> forEach, map, find, filter
+ ---------------------
+ template string 
+*/
